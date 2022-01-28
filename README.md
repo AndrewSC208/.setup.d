342435570f06f3c78d389e23b75587d76a44aa06
AndrewSC208 <meilingandrew@gmail.com>
2022-01-28 08:19:39 -0800

    setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setup.d
+A repo to automate setup of my development environment.
+
+It's simple run
+```shell
+$ sudo ./setup.sh
+```
---
+++ install-zsh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install zsh
+apt install zsh
+
+# install oh-my-zsh a plugin for zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
